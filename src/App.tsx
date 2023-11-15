3691ea40eaa7925bc218181e6b2553c165e24d14
Joey Pedicini <joey.pedicini@gmail.com>
2023-11-15 09:49:55 -0500

    basic setup with webmidi

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { Input, Note, Output, WebMidi } from "webmidi";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [inputs, setInputs] = useState<Input[]>([])
+  const [outputs, setOutputs] = useState<Output[]>([])
+  const [currentNote, setCurrentNote] = useState<Note>()
+
+  WebMidi
+    .enable()
+    .then(onEnabled)
+    .catch(err => alert(err));
+
+  function onEnabled() {
+
+    setInputs(WebMidi.inputs)
+    setOutputs(WebMidi.outputs)
+    // Inputs
+    WebMidi.inputs.forEach(input => {
+      console.log(input.manufacturer, input.name)
+      input.addListener('noteon', e => {
+        console.log(e.note.name + e.note.octave)
+        setCurrentNote(e.note);
+
+      });
+
+      // Outputs
+      WebMidi.outputs.forEach(output => {
+        console.log(output.manufacturer, output.name)
+        output.playNote("C4", {
+          duration: 500,
+        });
+      });
+
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>WebMidi</h1>
+      <h2>Inputs</h2>
+      <ul>
+        {inputs.map(input => (
+          <li key={input.id}>{input.name}</li>
+        ))}
+      </ul>
+      <h2>Outputs</h2>
+      <ul>
+        {outputs.map(output => (
+          <li key={output.id}>{output.name}</li>
+        ))}
+      </ul>
+      <h2>Current Note</h2>
+      {currentNote && (
+        <div>
+          <div>{currentNote.name}</div>
+          <div>{currentNote.octave}</div>
+        </div>
+      )}
     </>
   )
 }
